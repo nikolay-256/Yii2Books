10f729a06657f4305773b2b1fe95e2e0e3bfbd07
nadamson <nikolay-256@yandex.ru>
2020-05-04 07:07:31 +0300

    btns

--- views/site/index.php
+++ views/site/index.php
@@ -6,6 +6,8 @@ $this->title = 'Books&Authors';
 
 use yii\helpers\Html; ?>
 <div class="site-index">
-	<?= Html::a('Books list(with authors)', ['/book/index'], ['class'=>'btn btn-primary']) ?>
-	<?= Html::a('Authors list(with books count)', ['/author/index'], ['class'=>'btn btn-primary']) ?>
+	<?= Html::a('Authors list (with books count)', ['/author/index'], ['class'=>'btn btn-primary']) ?>
+	<span style="margin-right: 20px;"></span>
+	<?= Html::a('Books list (with authors)', ['/book/index'], ['class'=>'btn btn-primary']) ?>
+	
 </div>
